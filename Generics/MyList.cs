606f8b66cbb92f0cdab92b5a646b3281997d8258
Eda Derya Toper <edaderyatoper@gmail.com>
2023-02-24 15:10:23 +0300

    Add project files.

---
+++ Generics/MyList.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generics
+{
+    class MyList <T>
+    {
+        T[] items;
+
+        public MyList()
+        {
+            items = new T[0];
+        }
+        public void Add(T item)
+        {
+            T[] tempArray = items;
+            items = new T[items.Length + 1];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                tempArray[i] = items[i];
+            }
+            items[items.Length - 1] = item;
+        }
+        public int Count 
+        {
+            get { return items.Length; } 
+        }
+    }
+}
